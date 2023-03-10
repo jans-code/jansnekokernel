1b87149c0651497414bc062d3e9e2d802766a333
jans-code <7jantb@gmx.de>
2023-03-10 19:53:05 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# jansnekokernel
+
+![Logo](jansnekokernel/logo-svg.svg)
+
+A rough jupyter kernel implementation of [neko](https://www.nekovm.org).
+
+## Installation
+
+- install neko from your package repository
+- get the kernel module via pip
+- `pip install jansnekokernel`
+- then install kernelspec
+- `jansnekokernel`
+
+## Dev Installation
+
+- download/clone this project
+- open shell in project folder
+- `pip install -e ./`
+- `jupyter kernelspec install --user jansnekokernel`
+
+## Uninstall
+
+- `jupyter kernelspec uninstall jansnekokernel`
+- `pip uninstall jansnekokernel`
---
+++ src/jansnekokernel/kernel.py
@@ -0,0 +1,34 @@
+##!/usr/bin/env python
+from ipykernel.kernelbase import Kernel
+import pexpect, os, shutil
+
+class jansnekokernel(Kernel):
+    implementation = 'IPython'
+    implementation_version = '8.10.0'
+    language = 'neko'
+    language_version = '2.3.0'
+    language_info = {
+        'name': 'neko',
+        'mimetype': 'application/neko',
+        'file_extension': ['.neko', '.n'],
+    }
+    banner = "neko kernel"
+
+    def do_execute(self, code, silent, store_history=True, user_expressions=None,
+                   allow_stdin=False):
+        if not silent:            
+            workingdir = "/tmp/nekocode/"
+            os.mkdir(workingdir)
+            with open(workingdir + "project.neko", "w") as f:
+                    f.write(code)
+            pexpect.run('nekoc ' + workingdir  + 'project.neko')
+            solution = pexpect.run('neko ' + workingdir + 'project.n').decode('ascii')
+            shutil.rmtree(workingdir)
+            stream_content = {'name': 'stdout', 'text': solution}
+            self.send_response(self.iopub_socket, 'stream', stream_content)
+
+        return {'status': 'ok',
+                'execution_count': self.execution_count,
+                'payload': [],
+                'user_expressions': {},
+               }
